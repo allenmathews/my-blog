a91c200d638d45b4a9a334f37ec947dcd895ab11
Allen Mathews <allenjoemathews@gmail.com>
2021-12-09 16:59:05 +1100

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomePage = () => (
+<>
+  <h1> Hello, Welcome to my Blog </h1>
+  <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure nostrum illum earum ad architecto!
+  </p>
+  <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure nostrum illum earum ad architecto!
+  </p>
+  <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure nostrum illum earum ad architecto!
+  </p>
+  </>
+)
+
+export default HomePage;
